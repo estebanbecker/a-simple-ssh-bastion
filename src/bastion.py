6c942de6637ba1e1de698f00e075474a984da2b7
Tatsuya28 <ioiodauphin880@gmail.com>
2024-11-18 18:35:26 -0500

    Ajout de la journalisation de l'authentification, des actions utilisateurs, des retours serveurs, des erreurs

--- src/bastion.py
+++ src/bastion.py
@@ -4,6 +4,10 @@ from binascii import hexlify
 import socket
 import threading
 import sys
+import re
+import logging
+import os
+
 
 # Configuration des serveurs cibles
 servers = {
@@ -19,9 +23,21 @@ user = {
 }
 
 # setup logging
-paramiko.util.log_to_file("demo_server.log")
+# paramiko.util.log_to_file("demo_server.log")
+# Configuration de la journalisation globale
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("bastion.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+logger = logging.getLogger("BastionSSH")
+
 
 host_key = paramiko.RSAKey(filename="test_rsa.key")
+logger.info("Read key: %s", hexlify(host_key.get_fingerprint()))
 # host_key = paramiko.DSSKey(filename='test_dss.key')
 
 print("Read key: " + str(hexlify(host_key.get_fingerprint())))
@@ -40,6 +56,7 @@ def get_public_key(public_key_file):
             elif key_data.startswith("ecdsa-sha2-nistp256"):
                 return paramiko.ECDSAKey(data=base64.b64decode(key_data.split()[1]))
     except Exception as e:
+        logger.error(f"Erreur lors de la lecture de la clé publique: {e}")
         print(f"Erreur lors de la lecture de la clé publique: {e}")
         return None
 
@@ -57,8 +74,6 @@ def send_ascii_art(channel):
 >>=======================================================================================<<\r
 """
     channel.send(ascii_art + "\r\n")
-
-import re
 
 def visible_length(text):
     """Calcule la longueur visible d'une chaîne en ignorant les séquences ANSI."""
@@ -90,6 +105,26 @@ def print_table(servers, user, channel):
     channel.send(f"\r\n")
     return correspondance
 
+def get_user_logger(username):
+    logs_dir = "./logs"
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+
+    log_file = os.path.join(logs_dir, f"{username}.log")
+
+    logger = logging.getLogger(username)
+    logger.setLevel(logging.INFO)
+
+    # Si le logger a déjà des handlers, ne pas en rajouter
+    if not logger.handlers:
+        handler = logging.FileHandler(log_file)
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+
 class SSHServer(paramiko.ServerInterface):
 
     def __init__(self):
@@ -102,6 +137,7 @@ class SSHServer(paramiko.ServerInterface):
 
     def check_auth_password(self, username, password):
         if username in user and password == user[username]['password']:
+            logger.info(f"Authentification par mot de passe réussie pour {username}")
             print(f"Authentification par mot de passe réussie pour {username}")
             return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
@@ -123,10 +159,12 @@ class SSHServer(paramiko.ServerInterface):
             
             # Comparer les empreintes digitales des clés
             if key.get_fingerprint() == stored_key.get_fingerprint():
+                logger.info(f"Clé publique vérifiée pour {username}")
                 print(f"Clé publique vérifiée pour {username}")
                 return paramiko.AUTH_SUCCESSFUL
         except Exception as e:
-            print(f"Erreur lors de la vérification de la clé publique: {e}")
+            logger.error(f"Erreur lors de la vérification de la clé publique pour {username}: {e}") 
+            print(f"Erreur lors de la vérification de la clé publique pour {username}: {e}")
         
         return paramiko.AUTH_FAILED
 
@@ -173,6 +211,7 @@ class Connexion(threading.Thread):
             try:
                 transport.load_server_moduli()
             except:
+                logger.warning("Impossible de charger les moduli -- gex ne sera pas pris en charge.")
                 print("(Failed to load moduli -- gex will be unsupported.)")
                 raise
 
@@ -181,15 +220,22 @@ class Connexion(threading.Thread):
 
             try:
                 transport.start_server(server=server)
-            except paramiko.SSHException:
+            except paramiko.SSHException as e:
+                logger.error(f"Echec de la négociation SSH: {e}")
                 print("*** SSH negotiation failed.")
                 sys.exit(1)
 
             # Ouvrir un canal de session
             channel = transport.accept(20)
             if channel is None:
+                logger.error("Pas de canal ouvert pour la connexion.")
                 print("*** No channel.")
                 sys.exit(1)
+            # Authentification réussie
+            username = transport.get_username()
+            user_logger = get_user_logger(username)  # Initialiser le logger utilisateur
+            user_logger.info(f"Authentification réussie pour {username}")
+            user_logger.info(f"Utilisateur authentifié: {username}")
             print("Authenticated!")
 
             server.event.wait(10)
@@ -206,15 +252,18 @@ class Connexion(threading.Thread):
             channel.send("Sélectionnez un serveur:\r\n")
 
             correspondance = print_table(servers, user, channel)
+            logger.info(f"Serveurs listés pour {username}.")
 
             channel.send(f"Veuillez selectionner un serveur entre 1 et {len(correspondance)}: ")
                          
             # Lire la sélection de l'utilisateur
             server_choice = channel.recv(1024).decode('utf-8').strip()
+            user_logger.info(f"Sélection du serveur: {server_choice}")
 
             channel.send(f"\r\n")
 
             if server_choice not in correspondance:
+                user_logger.warning(f"Sélection invalide: {server_choice}")
                 channel.send("\033[91mErreur: Sélection invalide.\033[0m\r\n")
                 channel.close()
                 return
@@ -223,6 +272,7 @@ class Connexion(threading.Thread):
             target_server = servers[correspondance[server_choice]]
 
             channel.send(f"Connexion au serveur {target_server['hostname']} ...\r\n")
+            user_logger.info(f"Connexion au serveur {target_server['hostname']} ...")
 
             # Connexion au serveur cible
             target_transport = paramiko.Transport((target_server['hostname'], target_server['port']))
@@ -232,6 +282,7 @@ class Connexion(threading.Thread):
             target_channel.invoke_shell()
 
             # Transmettre les commandes et les résultats entre le client et le serveur cible
+            # et logger les commandes de l'utilisateur et les réponses du serveur
             def forward_data(source_channel, dest_channel):
                 try:
                     while True:
@@ -239,8 +290,10 @@ class Connexion(threading.Thread):
                         if not data:
                             break
                         dest_channel.send(data)
+                        user_logger.info(data.decode('utf-8'))
                 except Exception as e:
                     print(f"Erreur de transfert de données: {e}")
+                    user_logger.error(f"Erreur de transfert de données: {e}")
                 finally:
                     source_channel.close()
                     dest_channel.close()
@@ -251,17 +304,23 @@ class Connexion(threading.Thread):
 
             # Attendre la fin de la session
             target_channel.recv_exit_status()
+            user_logger.info(f"Déconnexion du serveur cible: {target_server['hostname']}")
             channel.send("\033[91mDéconnexion du serveur cible.\033[0m\r\n")
             channel.close()
 
         except paramiko.SSHException as e:
+            user_logger.error(f"Impossible de se connecter à {target_server['hostname']}: {e}")
             channel.send(f"Unable to connect to {target_server['hostname']}: {e}\r\n")
             channel.close()
             print(f"Erreur: {e}")
         except Exception as e:
+            user_logger.error(f"Erreur: {e}")
             print(f"Erreur: {e}")
         finally:
             self.client_socket.close()
+            user_logger.info("Session terminée.")
+            user_logger.info("Déconnexion du client.")
+            user_logger.info("Fermeture de la connexion.")
 
 class Bastion:
     def __init__(self, host, port):
@@ -276,14 +335,18 @@ class Bastion:
         server_socket.listen(5)
 
         print(f"Bastion SSH en écoute sur {self.host}:{self.port}")
+        logger.info(f"Bastion SSH en écoute sur {self.host}:{self.port}")
 
         try:
             while True:
                 client_socket, addr = server_socket.accept()
                 print(f"Connexion acceptée de {addr[0]}:{addr[1]}")
+                logger.info(f"Connexion acceptée de {addr[0]}:{addr[1]}")   
                 connexion = Connexion(client_socket)
                 threading.Thread(target=connexion.handle_client).start()
         except Exception as e:
-            print(f"Erreur: {e}")
+            print(f"Erreur serveur: {e}")
+            logger.error(f"Erreur serveur: {e}")
         finally:
             server_socket.close()
+            logger.info("Serveur arrêté.")
